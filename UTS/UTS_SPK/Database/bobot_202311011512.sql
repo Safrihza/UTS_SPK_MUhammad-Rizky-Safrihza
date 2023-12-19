6425747ba50b882f881c386b849e7e535a6b9515
Safrihza <149575172+Safrihza@users.noreply.github.com>
2023-12-19 23:48:07 +0700

    Add files via upload

---
+++ UTS/UTS_SPK/Database/bobot_202311011512.sql
@@ -0,0 +1,11 @@
+INSERT INTO "UTS_Spk".bobot (nama_tv,reputasi_brand,resolusi_layar,daya,harga,ukuran_layar) VALUES
+	 ('Sharp AQUOS LC-50LE275X',4,1,1,4,3),
+	 ('Samsung UA50TU8000',4,4,3,4,3),
+	 ('LG 49LH540T',5,3,3,3,3),
+	 ('Panasonic TH-55E306G',3,1,3,3,2),
+	 ('LG 49UK6300',5,2,2,3,1),
+	 ('Samsung UA43KU6000',4,4,1,2,3),
+	 ('Sharp LC-50SA5200X',4,1,3,2,3),
+	 ('TCL 43Q725',2,1,2,1,1),
+	 ('Samsung UA49J5200',4,5,2,1,3),
+	 ('CooCaa 50E3000T',1,1,3,1,2);
---
+++ UTS/UTS_SPK/Database/tv_202311011511.sql
@@ -0,0 +1,11 @@
+INSERT INTO "UTS_Spk".tv (nama_tv,reputasi_brand,resolusi,daya,harga,ukuran_layar) VALUES
+	 ('Sharp AQUOS LC-50LE275X','Terkenal','720P','60 W',3099000.00,37.00),
+	 ('Samsung UA50TU8000','Terkenal','4K','30 W',3099000.00,50.00),
+	 ('LG 49LH540T','Sangat Terkenal','1080P','40 W',3299000.00,49.00),
+	 ('Panasonic TH-55E306G','Cukup Terkenal','1080P','35 W',3499000.00,55.00),
+	 ('LG 49UK6300','Sangat Terkenal','4K','45 W',3499000.00,49.00),
+	 ('Samsung UA43KU6000','Terkenal','4K','55 W',3699000.00,43.00),
+	 ('Sharp LC-50SA5200X','Terkenal','1080P','35 W',3699000.00,50.00),
+	 ('TCL 43Q725','Lumayan Terkenal','4K','50 W',3999000.00,42.50),
+	 ('Samsung UA49J5200','Terkenal','2K','48 W',3999000.00,49.00),
+	 ('CooCaa 50E3000T','Kurang Terkenal','1080p','35 W',3999000.00,50.00);
